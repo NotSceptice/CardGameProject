1a901ebd1d1dab6d2d5ae1bb1592c5ff70085186
MARK JAMES GUEMO <markjamesg@live.wsd1.org>
2026-05-11 10:18:37 -0500

    added new card, still working on it

---
+++ src/OrderRvrseCard.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class OrderRvrseCard extends Card implements ReverseOrder{
+
+    public OrderRvrseCard(){
+        int maxPoint = 0;
+        int minPoint = 0;
+        int pointValue = Rand.randomInt(minPoint, maxPoint + 1);
+        super(pointValue);
+    }
+
+    @Override
+    public void play(Player currentPlayer, ArrayList<Player> allPlayers) {
+        currentPlayer.addPoints(super.getPointValue());
+
+        System.out.println(currentPlayer.getName() + " played " + this);
+        System.out.println(currentPlayer.getName() + " now has " + currentPlayer.getNumPoints() + " points.");
+    }
+
+    @Override
+    public void reverseOrder(ArrayList<Player> allPlayers){
+        Collections.reverse(allPlayers);
+
+        System.out.println("The player order has been reversed!");
+
+        for (Player player : allPlayers) {
+            System.out.println(player.getName());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Order Reverse Card { point value: " + super.getPointValue() + "}";
+    }
+}
---
+++ src/ReverseOrder.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public interface ReverseOrder {
+    public void reverseOrder(ArrayList<Player> allPlayers);
+}
