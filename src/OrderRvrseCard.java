bc4abc633eec383b16266b8630796dff18122101
MARK JAMES GUEMO <markjamesg@live.wsd1.org>
2026-05-20 10:20:04 -0500

    done reverse card (i think))

--- src/OrderRvrseCard.java
+++ src/OrderRvrseCard.java
@@ -14,6 +14,7 @@ public class OrderRvrseCard extends Card implements ReverseOrder{
     public void play(Player currentPlayer, ArrayList<Player> allPlayers) {
         currentPlayer.addPoints(super.getPointValue());
 
+
         System.out.println(currentPlayer.getName() + " played " + this);
         System.out.println(currentPlayer.getName() + " now has " + currentPlayer.getNumPoints() + " points.");
     }
@@ -23,10 +24,12 @@ public class OrderRvrseCard extends Card implements ReverseOrder{
         Collections.reverse(allPlayers);
 
         System.out.println("The player order has been reversed!");
+        System.out.println("New Order: ");
 
         for (Player player : allPlayers) {
-            System.out.println(player.getName());
+            System.out.println(player.getName() + " ");
         }
+        System.out.println();
     }
 
     @Override
